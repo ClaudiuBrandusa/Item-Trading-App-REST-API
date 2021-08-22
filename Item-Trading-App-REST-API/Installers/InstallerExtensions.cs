44bf6ec91937fd0ec1e2e950ee83ce954db37822
ClaudiuBrandusa <claudiu.office99@gmail.com>
2021-08-22 15:38:36 +0300

    Implement installers & Change launch settings

---
+++ Item-Trading-App-REST-API/Installers/InstallerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Item_Trading_App_REST_API.Installers
+{
+    public static class InstallerExtensions
+    {
+        public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(installer => typeof(IInstaller).IsAssignableFrom(installer) && !installer.IsInterface && !installer.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+
+            installers.ForEach(installer => installer.InstallServices(services, configuration));
+        }
+    }
+}
--- Item-Trading-App-REST-API/Startup.cs
+++ Item-Trading-App-REST-API/Startup.cs
@@ -1,8 +1,11 @@
+using Item_Trading_App_REST_API.Installers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
 
 namespace Item_Trading_App_REST_API
 {
@@ -18,7 +21,7 @@ namespace Item_Trading_App_REST_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
