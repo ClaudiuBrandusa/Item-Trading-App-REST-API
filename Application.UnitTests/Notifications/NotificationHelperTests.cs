d5271c402732b6d4975bb756193408860f597b57
claudiubrandusa <claudiu.office99@gmail.com>
2025-05-24 18:34:30 +0300

    Implement unit tests on the Application layer for NotificationHelper

---
+++ Application.UnitTests/Notifications/NotificationHelperTests.cs
@@ -0,0 +1,70 @@
+﻿using Application.Utils.Notifications;
+using Application.Utils.Notifications.NotificationStrategies;
+using Domain.Entities.Identity;
+
+namespace Application_UnitTests.Notifications;
+
+public class NotificationHelperTests
+{
+    [Fact(DisplayName = "Create single user notification strategy using the notification helper")]
+    public void CreateSingleUserNotificationStrategy()
+    {
+        // Arrange
+
+        string userId = User.GenerateId();
+
+        // Act
+
+        var notificationStrategy = NotificationHelper.CreateSingleUserNotificationStrategy(userId);
+
+        // Assert
+
+        Assert.IsType<NotifySingleUserStrategy>(notificationStrategy);
+    }
+
+    [Fact(DisplayName = "Create multiple users notification strategy using the notification helper")]
+    public void CreateMultipleUsersNotificationStrategy()
+    {
+        // Arrange
+
+        string[] userIds = new string[] { User.GenerateId(), User.GenerateId() };
+
+        // Act
+
+        var notificationStrategy = NotificationHelper.CreateMultipleUsersNotificationStrategy(userIds);
+
+        // Assert
+
+        Assert.IsType<NotifyMultipleUsersStrategy>(notificationStrategy);
+    }
+
+    [Fact(DisplayName = "Create all users notification strategy using the notification helper")]
+    public void CreateAllUsersNotificationStrategy()
+    {
+        // Arrange
+        
+        // Act
+
+        var notificationStrategy = NotificationHelper.CreateAllUsersNotificationStrategy();
+
+        // Assert
+
+        Assert.IsType<NotifyAllUsersStrategy>(notificationStrategy);
+    }
+
+    [Fact(DisplayName = "Create all users notification strategy using the notification helper")]
+    public void CreateAllUsersExceptNotificationStrategy()
+    {
+        // Arrange
+
+        string userId = User.GenerateId();
+
+        // Act
+
+        var notificationStrategy = NotificationHelper.CreateAllUsersExceptNotificationStrategy(userId);
+
+        // Assert
+
+        Assert.IsType<NotifyAllUsersExceptStrategy>(notificationStrategy);
+    }
+}
--- Application.UnitTests/Notifications/NotifyUserStrategyBuilderTests.cs
+++ Application.UnitTests/Notifications/NotifyUserStrategyBuilderTests.cs
@@ -2,11 +2,6 @@
 using Application.Utils.Notifications;
 using Application.Utils.Notifications.NotificationStrategies;
 using Domain.Entities.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application_UnitTests.Notifications;
 
