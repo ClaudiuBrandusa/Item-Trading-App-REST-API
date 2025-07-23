b1a9a5e446eee4b20faa9bb615e7b09b663b67dd
claudiubrandusa <claudiu.office99@gmail.com>
2025-07-23 20:40:08 +0300

    Add domain layer unit tests project | Add items domain layer unit tests

--- Domain/Entities/Items/Item.cs
+++ Domain/Entities/Items/Item.cs
@@ -2,6 +2,8 @@
 
 public class Item : Entity
 {
+    public const int MinimumNameLength = 3;
+
     public string ItemId { get; private set; }
 
     public string Name { get; private set; }
@@ -9,11 +11,17 @@ public class Item : Entity
     public string Description { get; private set; } = string.Empty;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    private Item() {}
+    private Item() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     public Item(string itemId, string name, string description = "")
     {
+        ArgumentException.ThrowIfNullOrEmpty(itemId, nameof(ItemId));
+        ArgumentException.ThrowIfNullOrEmpty(name, nameof(Name));
+
+        if (!ValidateName(name))
+            ThrowNameLengthException();
+
         ItemId = itemId;
         Name = name;
         Description = description;
@@ -23,11 +31,26 @@ public class Item : Entity
     {
     }
 
-    public void UpdateItemName(string itemName) => Name = itemName;
+    public void UpdateItemName(string itemName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(itemName, nameof(Name));
+
+        if (!ValidateName(itemName))
+            ThrowNameLengthException();
+
+        Name = itemName;
+    }
 
     public void UpdateItemDescription(string itemDescription) => Description = itemDescription;
 
     public static string GenerateId() => Guid.NewGuid().ToString();
+
+    private bool ValidateName(string name)
+    {
+        return name.Length >= MinimumNameLength;
+    }
+
+    private void ThrowNameLengthException() => throw new ArgumentException($"{nameof(Name)} length needs to be at least 3 characters.");
 
     protected override bool Compare(object obj)
     {
